b38af0bdcb58b7c9cc35330fd32354d39e0b9b4b
FunkyCracky <futebol4@hotmail.com>
2021-12-01 12:25:45 +0000

    Day 1 done

---
+++ Day01/day01.py
@@ -0,0 +1,12 @@
+def solve(x, k):
+    count = 0
+    for i in range(k,len(x)):
+        if x[i] > x[i-k]:
+            count += 1
+    return count
+
+f = open("input.txt", "r")
+input = [int(x) for x in f.readlines()]
+print(solve(input, 1)) #Part 1: 1548
+print(solve(input, 3)) #Part 2: 1589
+f.close()
