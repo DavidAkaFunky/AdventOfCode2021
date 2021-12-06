bab93997fa55dda3266ccae754a1385aa8a6c654
FunkyCracky <futebol4@hotmail.com>
2021-12-06 12:33:48 +0000

    Day 6 done!

---
+++ Day06/day06.py
@@ -0,0 +1,24 @@
+def solve(input, days, max, respawn):
+    fish = {x: 0 for x in range(0, max + 1)}
+    for i in input:
+        fish[i] += 1
+    for _ in range(days):
+        tmp = fish[0]
+        for i in range(max):
+            fish[i] = fish[i+1]
+        fish[max] = tmp
+        fish[respawn] += tmp
+    total = 0
+    for i in range(max + 1):
+        total += fish[i]
+    return total
+
+def main():
+    f = open("input.txt", "r")
+    input = [int(x) for x in f.readline().split(",")]
+    print(solve(input, 80, 8, 6)) #Part 1: 365862
+    print(solve(input, 256, 8, 6)) #Part 2: 1653250886439
+    f.close()
+
+if __name__ == "__main__":
+    main()
