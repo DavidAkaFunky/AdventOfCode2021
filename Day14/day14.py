cb43f017275a128c5f64f87a7766581ee751cd34
FunkyCracky <futebol4@hotmail.com>
2021-12-14 11:54:25 +0000

    Now it's a bit more clean :P

--- Day14/day14.py
+++ Day14/day14.py
@@ -8,38 +8,25 @@ def process(input):
 def solve(string, changes, steps):
     from copy import deepcopy
     import numpy as np
+    def insert_or_add(dict, key, value):
+        try:
+            dict[key] += value
+        except:
+            dict[key] = value
     def step(pairs, counts, changes):
         new_pairs = {}
         for pair in pairs:
             if pair in changes:
                 value = pairs[pair]
-                try:
-                    new_pairs[pair[0] + changes[pair]] += value
-                except KeyError:
-                    new_pairs[pair[0] + changes[pair]] = value
-                try:
-                    new_pairs[changes[pair] + pair[1]] += value
-                except KeyError:
-                    new_pairs[changes[pair] + pair[1]] = value
-                try:
-                    counts[changes[pair]] += value
-                except KeyError:
-                    counts[changes[pair]] = value
+                insert_or_add(new_pairs, pair[0] + changes[pair], value)
+                insert_or_add(new_pairs, changes[pair] + pair[1], value)
+                insert_or_add(counts, changes[pair], value)
         return new_pairs, counts
     counts, pairs = {}, {}
     for i in range(len(string)-1):
-        try: 
-            pairs[string[i] + string[i+1]] += 1
-        except KeyError:
-            pairs[string[i] + string[i+1]] = 1
-        try:
-            counts[string[i]] += 1
-        except KeyError:
-            counts[string[i]] = 1
-    try: 
-        counts[string[len(string)-1]] += 1
-    except KeyError:
-        counts[string[len(string)-1]] = 1 
+        insert_or_add(pairs, string[i] + string[i+1], 1)
+        insert_or_add(counts, string[i], 1)
+    insert_or_add(counts, string[len(string)-1], 1)
     new_pairs = deepcopy(pairs)
     for _ in range(steps):
         new_pairs, counts = step(new_pairs, counts, changes)
