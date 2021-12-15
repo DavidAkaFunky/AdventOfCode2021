dc2827fb84b9da0263abe7ebadb7609907df271b
FunkyCracky <futebol4@hotmail.com>
2021-12-15 22:31:01 +0000

    Day 15... Incomplete. F

---
+++ Day15/day15.py
@@ -0,0 +1,62 @@
+def solve(risks, source, target):
+    import numpy as np
+    def get_neighbours(graph, i, j):
+        neighbours = []
+        for x in (i-1, i+1):
+            if 0 <= x < len(graph):
+                neighbours.append((x,j))
+        for y in (j-1, j+1):
+            if 0 <= y < len(graph[i]):
+                neighbours.append((i,y))
+        return neighbours
+    dist, queue = [], []
+    for x in range(len(risks)):
+        dist.append([float("inf") for _ in range(len(risks[x]))])
+        for y in range(len(risks[x])):
+            queue.append((x, y))
+    dist[source[0]][source[1]] = 0
+    while queue != []:
+        lst = np.array([dist[x][y] for (x,y) in queue])
+        x = np.where(lst==np.min(lst))[0][0]
+        u = queue.pop(x)
+        if u == target:
+            return dist[u[0]][u[1]]
+        for v in get_neighbours(risks, u[0], u[1]):
+            if v in queue:
+                dist[v[0]][v[1]] = min(dist[u[0]][u[1]] + risks[v[0]][v[1]], dist[v[0]][v[1]])
+
+def expand_graph(graph, factor):
+    from copy import deepcopy
+    def increment_line(line):
+        line = deepcopy(line)
+        for i in range(len(line)):
+            line[i] = 1 if line[i] == 9 else line[i] + 1
+        return line
+    def increment_graph(graph):
+        graph = deepcopy(graph)
+        for i in range(len(graph)):
+            for j in range(len(graph[i])):
+                graph[i][j] = 1 if graph[i][j] == 9 else graph[i][j] + 1
+        return graph
+    new_graph, new_segment = deepcopy(graph), deepcopy(graph)
+    for _ in range(factor - 1):
+        new_segment = increment_graph(new_segment)
+        new_graph += new_segment
+    for i in range(len(new_graph)):
+        new_line = deepcopy(new_graph[i])
+        for _ in range(factor - 1):
+            new_line = increment_line(new_line)
+            new_graph[i] += new_line
+    return new_graph
+
+def main():
+    import numpy as np
+    f = open("input.txt", "r")
+    graph = [[int(x) for x in line.strip()] for line in f.readlines()]
+    print(solve(graph, (0,0), (len(graph[0]) - 1, len(graph) - 1))) #Part 1: 595
+    new_graph = expand_graph(graph, 5)
+    print(solve(new_graph, (0,0), (len(new_graph[0]) - 1, len(new_graph) - 1))) #Part 2: ???
+    f.close()
+
+if __name__ == "__main__":
+    main()
