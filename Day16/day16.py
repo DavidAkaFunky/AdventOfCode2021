6df7332c4f8f4f9a1e486146c5bbfb39f3d53df0
FunkyCracky <futebol4@hotmail.com>
2021-12-17 15:37:46 +0000

    Day 16 finally done WOOOOOOO

--- Day16/day16.py
+++ Day16/day16.py
@@ -1,4 +1,4 @@
-def solve(number):
+def solve(number, string, ops, flag = False, count = 0):
     from time import sleep
     i = 0
     sum = 0
@@ -8,32 +8,58 @@ def solve(number):
         sum += packet_version
         if packet_id == 4:
             j = 6
+            bits = ""
             while i+j < len(number) and int(number[i+j]) == 1:
+                bits += number[i+j+1 : i+j+5]
                 j += 5
+            bits += number[i+j+1 : i+j+5]
+            string += str(int(bits, 2)) + ","
             j += 5
         else:
             len_id = int(number[i+6])
+            string += ops[packet_id]
             if len_id == 0:
                 len_sub = int(number[i+7 : i+22], 2)
-                sum += solve(number[i+22 : i+22+len_sub])[0]
+                (sub_sum, _, string) = solve(number[i+22 : i+22+len_sub], string, ops)
+                sum += sub_sum
                 j = 22 + len_sub
             elif len_id == 1:
                 subpackets = int(number[i+7 : i+18], 2)
-                k = 0
-                for l in range(subpackets):
-                    (sub_sum, add) = solve(number[i+k+18:])
+                k = 18
+                for _ in range(subpackets):
+                    (sub_sum, add, string) = solve(number[i+k:], string, ops, flag = True)
                     sum += sub_sum
                     k += add
-                j = 25 + k
+                j = k
+            string += "]),"
         i += j
-    return (sum, i)
+        if flag:
+            break
+    return (sum, i, string)
+
+def equal(lst):
+    if len(lst) != 2:
+        raise ValueError("equal" + str(len(lst)))
+    return 1 if lst[0] == lst[1] else 0
+
+def greater(lst):
+    if len(lst) != 2:
+        raise ValueError("greater" + str(len(lst)))
+    return 1 if lst[0] > lst[1] else 0
+
+def less(lst):
+    if len(lst) != 2:
+        raise ValueError("less" + str(len(lst)))
+    return 1 if lst[0] < lst[1] else 0
 
 def main():
+    from numpy import product
     f = open("input.txt", "r")
     number = bin(int("1" + f.readline(), 16))[3:]
-    (sum, _) = solve(number)
-    print(sum) #Part 1: 860
-    #print(total) #Part 2: ???
+    (sum_pack, _, expr) = solve(number, "", {0: "sum([", 1: "product([", 2: "min([", 3: "max([", 5: "greater([", 6: "less([", 7: "equal(["})
+    print(sum_pack) #Part 1: 860
+    expr = expr[:-1]
+    print(eval(expr)) #Part 2: 470949537659
     f.close()
 
 if __name__ == "__main__":
